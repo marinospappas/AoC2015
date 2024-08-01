55901cb93d29570da7fd4bf9026424b630dbb8f0
marinos <marinosch.99981@gmail.com>
2024-08-01 18:37:48 +0200

    Day 07 skeleton

---
+++ src/main/kotlin/mpdev/springboot/aoc2015/solutions/day07/DigitalCircuit.kt
@@ -0,0 +1,16 @@
+package mpdev.springboot.aoc2015.solutions.day07
+
+import mpdev.springboot.aoc2015.input.InputDataReader
+import mpdev.springboot.aoc2015.solutions.PuzzleSolver
+import org.springframework.stereotype.Component
+
+@Component
+class DigitalCircuit(inputDataReader: InputDataReader): PuzzleSolver(inputDataReader, 7) {
+
+    override fun solvePart1() = 0
+
+    override fun solvePart2() = 0
+
+    companion object {
+    }
+}
---
+++ src/test/kotlin/mpdev/springboot/aoc2015/day07/Day07Test.kt
@@ -0,0 +1,47 @@
+package mpdev.springboot.aoc2015.day07
+
+import mpdev.springboot.aoc2015.input.InputDataReader
+import mpdev.springboot.aoc2015.solutions.day07.DigitalCircuit
+import mpdev.springboot.aoc2015.utils.println
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+
+class Day07Test {
+
+    private val day = 7                                    ///////// Update this for a new dayN test
+    private lateinit var solver: DigitalCircuit         ///////// Update this for a new dayN test
+    private val inputDataReader = InputDataReader("src/test/resources/inputdata/input")
+
+    @BeforeEach
+    fun setup() {
+        solver = DigitalCircuit(inputDataReader)
+        solver.initialize()
+    }
+
+    @Test
+    @Order(1)
+    fun `Sets Day correctly`() {
+        assertThat(solver.day).isEqualTo(day)
+    }
+
+    @Test
+    @Order(2)
+    fun `Reads Input and sets up Circuit`() {
+
+    }
+
+    @Test
+    @Order(4)
+    fun `Solves Part 1`() {
+        val result = solver.solvePart1().also { it.println() }
+        assertThat(result).isEqualTo(998996)
+    }
+
+    @Test
+    @Order(6)
+    fun `Solves Part 2`() {
+        val result = solver.solvePart2().also { it.println() }
+        assertThat(result).isEqualTo(1001996)  }
+}
