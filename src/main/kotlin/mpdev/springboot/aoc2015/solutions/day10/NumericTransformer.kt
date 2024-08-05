d92267e325edaa26d2f8b321a6c0522b873b9600
marinos <marinosch.99981@gmail.com>
2024-08-05 19:44:04 +0200

    Day 10 - using List<Char> instead of String for performance

---
+++ src/main/kotlin/mpdev/springboot/aoc2015/solutions/day10/NumericTransformer.kt
@@ -0,0 +1,46 @@
+package mpdev.springboot.aoc2015.solutions.day10
+
+import mpdev.springboot.aoc2015.input.InputDataReader
+import mpdev.springboot.aoc2015.solutions.PuzzleSolver
+import org.springframework.stereotype.Component
+
+@Component
+class NumericTransformer(inputDataReader: InputDataReader): PuzzleSolver(inputDataReader, 10) {
+
+    val startSequence =  inputData[0].toCharArray().toList()
+
+    override fun initialize() {
+    }
+
+    fun lookAndSay(s: List<Char>): List<Char> {
+        val result = mutableListOf<Char>()
+        var subSeq = mutableListOf<Char>()
+        for (i in s.indices) {
+            subSeq += s[i]
+            if (i == s.lastIndex || s[i + 1] != s[i]) {
+                result.addAll(lookAndSaySubSeq(subSeq))
+                subSeq = mutableListOf()
+            }
+        }
+        return result
+    }
+
+    fun lookAndSaySubSeq(s: List<Char>) =
+        s.size.toString().toCharArray().toList() + s[0]
+
+    lateinit var sequencePart1: List<Char>
+
+    override fun solvePart1(): Int {
+        var sequence = startSequence
+        repeat(40) { sequence = lookAndSay(sequence) }
+        sequencePart1 = sequence
+        return sequence.size
+    }
+
+    override fun solvePart2(): Int {
+        var sequence = sequencePart1
+        repeat(10) { sequence = lookAndSay(sequence) }
+        return sequence.size
+    }
+
+}
---
+++ src/test/kotlin/mpdev/springboot/aoc2015/day10/Day10Test.kt
@@ -0,0 +1,38 @@
+package mpdev.springboot.aoc2015.day10
+
+import mpdev.springboot.aoc2015.input.InputDataReader
+import mpdev.springboot.aoc2015.solutions.day10.NumericTransformer
+import mpdev.springboot.aoc2015.utils.println
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+
+class Day10Test {
+
+    private val day = 10                                    ///////// Update this for a new dayN test
+    private lateinit var solver: NumericTransformer         ///////// Update this for a new dayN test
+    private val inputDataReader = InputDataReader("src/test/resources/inputdata/input")
+
+    @BeforeEach
+    fun setup() {
+        solver = NumericTransformer(inputDataReader)
+        solver.initialize()
+    }
+
+    @Test
+    @Order(1)
+    fun `Sets Day correctly`() {
+        assertThat(solver.day).isEqualTo(day)
+    }
+
+    @Test
+    @Order(2)
+    fun `Executes Look and Tell`() {
+        val expected = listOf("312211", "11", "21",  "1211", "111221")
+        for (i in solver.inputData.indices) {
+            val result = solver.lookAndSay(solver.inputData[i].toCharArray().toList()).also { it.println() }
+            assertThat(result).isEqualTo(expected[i].toCharArray().toList())
+        }
+    }
+}
