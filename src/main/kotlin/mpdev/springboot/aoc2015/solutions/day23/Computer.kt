f85ad6b4a2aa8f1d7e733565c32bc5acca6f0b76
marinos <marinosch.99981@gmail.com>
2024-08-10 18:19:23 +0200

    Day 23 improved initialisation of OpCodes fo readability

--- src/main/kotlin/mpdev/springboot/aoc2015/solutions/day23/Computer.kt
+++ src/main/kotlin/mpdev/springboot/aoc2015/solutions/day23/Computer.kt
@@ -17,17 +17,9 @@ class Computer(inputDataReader: InputDataReader): PuzzleSolver(inputDataReader, 
     var debug = false
 
     override fun initialize() {
-        InstructionSet.opCodesList["hlf"] = InstructionSet.OpCode("hlf", 2, listOf(R, W))
-            { a -> Pair(OpResultCode.SET_MEMORY, listOf(a[1], a[0] as Long / 2L)) }
-        InstructionSet.opCodesList["tpl"] = InstructionSet.OpCode("tpl", 2, listOf(R, W))
-            { a -> Pair(OpResultCode.SET_MEMORY, listOf(a[1], a[0] as Long * 3L)) }
-        InstructionSet.opCodesList["jie"] = InstructionSet.OpCode("jie", 2, listOf(R, R))
-            { a -> if (a[0] as Long % 2L == 0L) Pair(OpResultCode.INCR_PC, listOf(a[1] as Long)) else
-                Pair(OpResultCode.NONE, emptyList()) }
-        InstructionSet.opCodesList["jio"] = InstructionSet.OpCode("jio", 2, listOf(R, R))
-            { a -> if (a[0] as Long == 1L) Pair(OpResultCode.INCR_PC, listOf(a[1] as Long)) else
-                Pair(OpResultCode.NONE, emptyList()) }
-        aocVm = AocVm(inputData.toMutableList().also { it.add(0, "in a") } .also { it.add("out b") }
+        initialiseOpCodes()
+        aocVm = AocVm(inputData
+            .toMutableList().also { it.add(0, "in a") } .also { it.add("out b") }
             .map { it.replace(", ", ",").replace(" ", ",") },
             debug = debug)
     }
@@ -49,9 +41,23 @@ class Computer(inputDataReader: InputDataReader): PuzzleSolver(inputDataReader, 
             aocVm.sendInputToProgram(1)
             val job = launch { aocVm.runProgram() }
             aocVm.waitProgram(job)
-            result = aocVm.getProgramRegister("b")
+            result = aocVm.getOutputFromProgram().last()
         }
         return result
     }
 
+    companion object {
+        fun initialiseOpCodes() {
+            InstructionSet.opCodesList["hlf"] = InstructionSet.OpCode("hlf", 2, listOf(R, W))
+            { a -> Pair(OpResultCode.SET_MEMORY, listOf(a[1], a[0] as Long / 2L)) }
+            InstructionSet.opCodesList["tpl"] = InstructionSet.OpCode("tpl", 2, listOf(R, W))
+            { a -> Pair(OpResultCode.SET_MEMORY, listOf(a[1], a[0] as Long * 3L)) }
+            InstructionSet.opCodesList["jie"] = InstructionSet.OpCode("jie", 2, listOf(R, R))
+            { a -> if (a[0] as Long % 2L == 0L) Pair(OpResultCode.INCR_PC, listOf(a[1] as Long)) else
+                Pair(OpResultCode.NONE, emptyList()) }
+            InstructionSet.opCodesList["jio"] = InstructionSet.OpCode("jio", 2, listOf(R, R))
+            { a -> if (a[0] as Long == 1L) Pair(OpResultCode.INCR_PC, listOf(a[1] as Long)) else
+                Pair(OpResultCode.NONE, emptyList()) }
+        }
+    }
 }
